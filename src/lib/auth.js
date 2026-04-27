35d31258ad3ba1698550783d57c48914f4647001
smRid <omorsk911@gmail.com>
2026-04-27 15:38:36 +0600

    initialize Better Auth configwith MongoDB adapter

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,7 +2,7 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient(process.env.MONGO_URI);
+const client = new MongoClient(process.env.MONGO_URI || "");
 const db = client.db("dragon-news");
 
 
