1fd798877d1038ce7e334d2cb19b5e69c0f443f2
smRid <omorsk911@gmail.com>
2026-04-27 15:40:42 +0600

    auth error fix

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -5,10 +5,8 @@ import { mongodbAdapter } from "better-auth/adapters/mongodb";
 const client = new MongoClient(process.env.MONGO_URI || "");
 const db = client.db("dragon-news");
 
-
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
-    // Optional: if you don't provide a client, database transactions won't be enabled.
     client,
   }),
 
